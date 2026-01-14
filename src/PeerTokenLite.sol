b526ab87711b22a0569b0a890f600e20cc1861fc
evgeniko <97796468+evgeniko@users.noreply.github.com>
2026-01-14 21:53:18 +0100

    use AccessControl for the PeerLite token

--- src/PeerTokenLite.sol
+++ src/PeerTokenLite.sol
@@ -4,40 +4,19 @@ pragma solidity >=0.6.12 <0.9.0;
 import {ERC20} from "openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
 import {ERC20Burnable} from "openzeppelin-contracts/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import {ERC20Permit} from "openzeppelin-contracts/contracts/token/ERC20/extensions/ERC20Permit.sol";
-import {Ownable} from "openzeppelin-contracts/contracts/access/Ownable.sol";
+import {AccessControl} from "openzeppelin-contracts/contracts/access/AccessControl.sol";
 
-contract PeerTokenLite is ERC20, ERC20Permit, ERC20Burnable, Ownable {
-    error CallerNotMinter(address caller);
-    error InvalidMinterZeroAddress();
+contract PeerTokenLite is ERC20, ERC20Permit, ERC20Burnable, AccessControl {
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
 
-    event NewMinter(address newMinter);
-
-    address public minter;
-
-    modifier onlyMinter() {
-        if (msg.sender != minter) {
-            revert CallerNotMinter(msg.sender);
-        }
-        _;
-    }
-
-    constructor(string memory _name, string memory _symbol, address _minter, address _owner)
+    constructor(string memory _name, string memory _symbol, address _admin)
         ERC20(_name, _symbol)
         ERC20Permit(_name)
-        Ownable(_owner)
     {
-        minter = _minter;
+        _grantRole(DEFAULT_ADMIN_ROLE, _admin);
     }
 
-    function mint(address _account, uint256 _amount) external onlyMinter {
+    function mint(address _account, uint256 _amount) external onlyRole(MINTER_ROLE) {
         _mint(_account, _amount);
-    }
-
-    function setMinter(address newMinter) external onlyOwner {
-        if (newMinter == address(0)) {
-            revert InvalidMinterZeroAddress();
-        }
-        minter = newMinter;
-        emit NewMinter(newMinter);
     }
 }
